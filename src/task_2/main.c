1866ff2ee2377b49165ef22cb6012e79c77ba9f0
Marjoram Mickie <marjoram@ga-c5.msk.21-school.ru>
2024-04-12 20:33:13 +0300

    t2

---
+++ src/task_2/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+  int R;
+  int P = 21500;
+  double m;
+
+  int main (){
+scanf("%d", &R);
+if(R>0){
+m=(P*4*M_PI*R*R*R)/3;
+    printf("%.0lf", m);}
+    else{printf("n/a");}
+return 0;
+  }
